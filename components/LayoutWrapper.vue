976568a08de2337bda93a57a32ee39f639bf717d
kento <kento@toriwatari.bird>
2021-04-30 13:30:24 +0900

    Update P286
    wrapperのコンポーネント化

---
+++ components/LayoutWrapper.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutWrapper',
+}
+</script>
+
+<style scoped>
+.wrapper {
+  padding-top: 8000px;
+  border: 1px solid red;
+  @media (min-width: 768px) {
+    padding-top: 0;
+  }
+}
+</style>
