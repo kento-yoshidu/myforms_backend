9f92c7de09c1122005bd3396b577cd32b118078a
kento <kento@toriwatari.bird>
2021-05-01 11:37:59 +0900

    Update P326

--- pages/menu/index.vue
+++ pages/menu/index.vue
@@ -1,6 +1,6 @@
 <template>
   <layout-wrapper>
-    <layout-visual title="MENU" :height="40" visual="visual-menu" />
+    <layout-visual title="MENU" :height="50" visual="visual-menu" />
     <div class="container w-7/12 mx-auto pt-20 pb-20 px-6 md:px-0">
       <div class="mb-20">
         <layout-menu-list />
