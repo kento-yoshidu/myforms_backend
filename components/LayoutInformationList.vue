91cd60afffd7d41cbc80ba1c0c6025b886af96a2
kento <kento@toriwatari.bird>
2021-05-01 19:11:50 +0900

    Update P377

--- components/LayoutInformationList.vue
+++ components/LayoutInformationList.vue
@@ -4,27 +4,17 @@
     <div
       class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 shadow-md mb-5"
     >
-      <a href="/information/detail" class="block px-4 py-3">
-        <time class="text-gray-700 text-base mb-1 block md:w-1/6">2021.05.01</time>
-        <div class="md:w-10/12">お知らせタイトル</div>
-      </a>
-    </div>
+      <a
+        href="'/information/' + id + '/'"
+        class="block px-4 py-3"
+      >
+        <time class="text-gray-700 text-base mb-1 block md:w-1/6">
+          {{ date | formatDate }}
+        </time>
 
-    <div
-      class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 shadow-md mb-5"
-    >
-      <a href="/information/detail" class="block px-4 py-3">
-        <time class="text-gray-700 text-base mb-1 block md:w-1/6">2021.05.01</time>
-        <div class="md:w-10/12">お知らせタイトル</div>
-      </a>
-    </div>
-
-    <div
-      class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 shadow-md mb-5"
-    >
-      <a href="/information/detail" class="block px-4 py-3">
-        <time class="text-gray-700 text-base mb-1 block md:w-1/6">2021.05.01</time>
-        <div class="md:w-10/12">お知らせタイトル</div>
+        <div class="md:w-10/12">
+          {{ title }}
+        </div>
       </a>
     </div>
 
@@ -33,6 +23,20 @@
 
 <script>
 export default {
-  name: 'LayoutInformationList'
+  name: 'LayoutInformationList',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: String,
+      required: true
+    },
+    id: {
+      type: String,
+      required: true
+    }
+  }
 }
 </script>
