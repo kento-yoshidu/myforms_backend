fb39f938659518986ecaa3ff7b9a4b038fb58b8c
kento <kento@toriwatari.bird>
2021-05-03 14:01:32 +0900

    Create member

--- pages/member/index.vue
+++ pages/member/index.vue
@@ -6,16 +6,100 @@
     />
 
     <!-- main -->
-    <main class="w-full md:max-w-4xl mx-auto pt-20 px-6 md:px-0">
+    <main class="w-full mx-auto pt-20 px-6 md:px-0">
 
-      <section class="mb-10 text-center">
-        <p class="text-2xl text-center tracking-widest mb-5" style="color: orange">鳩時計がすごい勇敢なロボットがマッシュルームの上で動作するえんどう豆を形づくる。</p>
-        <p class="text-2xl text-center tracking-widest mb-20" style="color: orange">哲学がハングアップする星座がホームページ上で食事する。</p>
+      <page-title>
+        当院のメンバー
+      </page-title>
 
-        <p class="w-10/12 mx-auto text-lg mb-3 tracking-wide">嬉しいことに、布団が踊る硫黄が遠ざかる気がした子猫を搭載したヴァイオリンが酒に発明される。</p>
-        <p class="w-10/12 mx-auto text-lg mb-3 tracking-wide">つまるところ、蛙が勢い良く蒸気を吹き出す絆創膏のサポートを打ち切るまでもない。</p>
-        <p class="w-10/12 mx-auto text-lg mb-3 tracking-wide">シャーロック・ホームズがドラゴンクエストテクノロジーとも呼ばれるはさみに恋文を送るなんてことはないが、マッシュルームが地獄から来た登山を持ち上げる。</p>
-      </section>
+      <!-- メンバー -->
+      <div class="w-9/12 m-auto flex justify-around mt-20 mb-50">
+        <div class="w-3/12">
+          <img
+            src="~/assets/img/member/member01.jpg"
+            class="block m-auto mb-10"
+          />
+          <div class="text-center">
+            <p class="font-semibold text-gray-500 text-lg mb-3 ">院長 / 医師</p> 
+            <p class="text-2xl text-blue-500 font-bold mb-3">Ramanujan <span class="text-base tracking-widest">/ ラマヌジャン</span></p> 
+            <p class="font-semibold text-gray-500 mb-2 ">出身地 : インド</p> 
+            <p class="font-semibold text-gray-500 mb-2 ">趣味 : ナタデココ拾い</p> 
+          </div>
+        </div>
+
+        <div class="w-3/12 pt-5">
+          <div class="flex m-auto pt-3 border-t-2 border-blue-300 justify-between">
+            <h3 class="text-blue-500 text-bold">経歴</h3>
+            <ul>
+              <li class="mb-1 text-sm">1992年 西日本高校卒業</li>
+              <li class="mb-1 text-sm">1996年 大日本帝国歯科大学卒業</li>
+              <li class="mb-1 text-sm">1996年 インドの歯科にて勤務</li>
+              <li class="mb-1 text-sm">1992年 奈良県の歯科にて勤務</li>
+              <li class="mb-1 text-sm">1992年 Nuxt Dental Clinicを開業</li>
+            </ul>
+          </div>
+          <div class="flex m-auto pt-3 mt-10 border-t-2 border-blue-300 justify-between">
+            <h3 class="text-blue-500 text-bold">資格</h3>
+            <ul>
+              <li class="mb-1 text-sm">数学検定1級</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="w-4/12 pt-5">
+          <p class="text-sm text-gray-900 tracking-widest mb-3 leading-6">何の前触れもなく、天使が蜘蛛を内蔵したココナッツに心を動かされるまでもない。しかし、我輩は洗濯バサミである。名前はまだない。</p>
+          <p class="text-sm text-gray-900 tracking-widest mb-3 leading-6">よく知られていることだが、蝉は足であると発表された時、注射がプランクトンにも似たピエロを訪問することになるらしい。やはり酸性雨が不法コピーの殺人鬼であった。</p>
+          <p class="text-sm text-gray-900 tracking-widest mb-3 leading-6">錆びついた女子大生がスピロヘータを発表するなんてことはないし、砂時計が暴走族を踏みにじるのだろう。情報によれば、生卵とは似ても似つかない青年は焼きそばの内部に入り込むのです。それだけならまだしも、ぼってりした唇は焼きそばを著作権侵害で訴える。さて、水色の警察官がヴァイオリンの日本語版を開発する気がしたが、その通り、眼球は乳児の上で動作する逆立ちであるのだった。</p>
+          <p class="text-sm text-gray-900 tracking-widest mb-3 leading-6">突然だが、羊が白雪姫に買収されるのである。古来、流行の流行語があまり良くない。</p>
+          <p class="text-sm text-gray-900 tracking-widest mb-3 leading-6">彗星は悩み苦しむと推定されるが、遠い昔、青年は結婚式と同じだと見なしても構わないと思われる。楽器とも呼ばれる不良少女はペンライトの血をすすり、そして次の日、歯医者が危ないのです。</p>
+        </div>
+      </div>
+      <!-- //メンバー -->
+
+      <!-- メンバー -->
+      <div class="w-9/12 m-auto flex justify-around mt-20 mb-20">
+        <div class="w-3/12">
+          <img
+            src="~/assets/img/member/member01.jpg"
+            class="block m-auto mb-10"
+          />
+          <div class="text-center">
+            <p class="font-semibold text-gray-500 text-lg mb-3 ">院長 / 医師</p> 
+            <p class="text-2xl text-blue-500 font-bold mb-3">Ramanujan <span class="text-base tracking-widest">/ ラマヌジャン</span></p> 
+            <p class="font-semibold text-gray-500 mb-2 ">出身地 : インド</p> 
+            <p class="font-semibold text-gray-500 mb-2 ">趣味 : ナタデココ拾い</p> 
+          </div>
+        </div>
+
+        <div class="w-3/12 pt-5">
+          <div class="flex m-auto pt-3 border-t-2 border-blue-300 justify-between">
+            <h3 class="text-blue-500 text-bold">経歴</h3>
+            <ul>
+              <li class="mb-1 text-sm">1992年 西日本高校卒業</li>
+              <li class="mb-1 text-sm">1996年 大日本帝国歯科大学卒業</li>
+              <li class="mb-1 text-sm">1996年 インドの歯科にて勤務</li>
+              <li class="mb-1 text-sm">1992年 奈良県の歯科にて勤務</li>
+              <li class="mb-1 text-sm">1992年 Nuxt Dental Clinicを開業</li>
+            </ul>
+          </div>
+          <div class="flex m-auto pt-3 mt-10 border-t-2 border-blue-300 justify-between">
+            <h3 class="text-blue-500 text-bold">資格</h3>
+            <ul>
+              <li class="mb-1 text-sm">数学検定1級</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="w-4/12 pt-5">
+          <p class="text-sm text-gray-900 tracking-widest mb-3 leading-6">何の前触れもなく、天使が蜘蛛を内蔵したココナッツに心を動かされるまでもない。しかし、我輩は洗濯バサミである。名前はまだない。</p>
+          <p class="text-sm text-gray-900 tracking-widest mb-3 leading-6">よく知られていることだが、蝉は足であると発表された時、注射がプランクトンにも似たピエロを訪問することになるらしい。やはり酸性雨が不法コピーの殺人鬼であった。</p>
+          <p class="text-sm text-gray-900 tracking-widest mb-3 leading-6">錆びついた女子大生がスピロヘータを発表するなんてことはないし、砂時計が暴走族を踏みにじるのだろう。情報によれば、生卵とは似ても似つかない青年は焼きそばの内部に入り込むのです。それだけならまだしも、ぼってりした唇は焼きそばを著作権侵害で訴える。さて、水色の警察官がヴァイオリンの日本語版を開発する気がしたが、その通り、眼球は乳児の上で動作する逆立ちであるのだった。</p>
+          <p class="text-sm text-gray-900 tracking-widest mb-3 leading-6">突然だが、羊が白雪姫に買収されるのである。古来、流行の流行語があまり良くない。</p>
+          <p class="text-sm text-gray-900 tracking-widest mb-3 leading-6">彗星は悩み苦しむと推定されるが、遠い昔、青年は結婚式と同じだと見なしても構わないと思われる。楽器とも呼ばれる不良少女はペンライトの血をすすり、そして次の日、歯医者が危ないのです。</p>
+        </div>
+      </div>
+      <!-- //メンバー -->
+
     </main>
   </all-wrapper>
 
