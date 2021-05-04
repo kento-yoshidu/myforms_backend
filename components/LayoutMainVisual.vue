f9290f81ddb020af15bad18076136c8d6dcbf4d3
kento <kento@toriwatari.bird>
2021-05-04 19:17:31 +0900

    WIP Update

--- components/LayoutMainVisual.vue
+++ components/LayoutMainVisual.vue
@@ -1,33 +1,27 @@
 <template>
   <!-- top -->
   <div
-    class="relative md:pt-16 pb-32 flex z-10"
+    class="relative mt-20 md:pt-16 pb-32 flex z-10"
     :style="`min-height: ${height}vh`"
   >
-    <div :class="`absolute top-0 right-0 w-full h-5/6 bg-center bg-cover ${visual}`">
+    <div :class="`absolute top-0 right-0 w-10/12 h-5/6 m-auto bg-center bg-cover ${visual}`">
       <span
         id="blackOverlay"
         class="w-full h-full absolute opacity-75"
       ></span>
     </div>
 
-    <div class="container relative mx-auto">
-      <div class="items-center flex flex-wrap">
-        <div class="w-full px-4 ml-auto mr-auto">
-          <div>
-            <!--<h1 class="text-gray-400 font-semibold text-5xl text-shadow">-->
-            <h1 class="filter drop-shadow-lg text-blue-500 font-sm text-4xl text-shadow">
-              {{ title }}
-            </h1>
-            <p
-              v-if="message" 
-              class="mt-4 text-2xl text-gray-400 text-shadow-md"
-            >
-              {{ message }}
-            </p>
-          </div>
-        </div>
-      </div>
+    <div class="absolute top-20 container relative mx-auto border-4 p-4">
+      <!--<h1 class="text-gray-400 font-semibold text-5xl text-shadow">-->
+      <h1 class="filter drop-shadow-lg text-blue-500 font-sm text-3xl md:text-4xl text-shadow">
+        {{ title }}
+      </h1>
+      <p
+        v-if="message" 
+        class="mt-4 text-2xl text-gray-700 text-shadow-md"
+      >
+        {{ message }}
+      </p>
     </div>
   </div>
 </template>
