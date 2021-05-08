fbb76cb63908331e35c84b95903935e8baa2d9ee
kento <kento@toriwatari.bird>
2021-05-08 14:06:29 +0900

    #41 Create LayoutBlogList.vue

---
+++ components/LayoutBlogList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+  <h2>ブログリストA</h2>
+    <p>{{title}}</p>
+    <p>{{date}}</p>
+  </div> 
+</template>
+
+<script>
+export default {
+  name: 'LayoutBlogList',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: String,
+      required: true
+    },
+    id: {
+      type: String,
+      required: true
+    },
+    picture: {
+      type: String,
+    }
+  }
+}
+</script>
