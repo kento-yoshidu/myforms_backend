43508dd8c00eb858b3a8fef722c0a0f3e3f300ba
kento <kento@toriwatari.bird>
2021-06-09 20:38:35 +0900

    Update information/index.bue

--- pages/information/old.vue
+++ pages/information/old.vue
@@ -1,7 +1,7 @@
 <template>
   <all-wrapper>
     <layout-main-visual
-      title="最新のお知らせ"
+      title="過去のお知らせ"
       visual="visual-information"
     />
 
@@ -9,7 +9,7 @@
     <main class="w-full md:max-w-4xl mx-auto px-6 md:px-0">
 
       <section class="mb-2 text-center">
-        <p class="text-lg md:text-2xl text-center tracking-widest mb-10" style="color: orange">最新のお知らせ一覧です。</p>
+        <p class="text-lg md:text-2xl text-center tracking-widest mb-10" style="color: orange">過去のお知らせ一覧です。</p>
       </section>
 
       <layout-information-list
@@ -22,8 +22,8 @@
       />
 
       <base-button
-        name="過去のお知らせを見る"
-        link="/information/old/"
+        name="最新のお知らせを見る"
+        link="/information/"
       />
     </main>
   </all-wrapper>
@@ -34,7 +34,7 @@ import axios from 'axios'
 
 export default {
   async asyncData({ $config }) {
-    const { data } = await axios.get(`${$config.apiUrl}/information?filters=isRecent[equals]true`, {
+    const { data } = await axios.get(`${$config.apiUrl}/information?filters=isRecent[equals]false`, {
       headers: { 'X-API-KEY': $config.apiKey }
     })
     return {
