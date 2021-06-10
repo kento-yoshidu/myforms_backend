0bb719ced429020e83f01acf54660edc001368b2
kento <kento@toriwatari.bird>
2021-06-10 20:17:15 +0900

    Update index.vue

--- components/LayoutInformationList.vue
+++ components/LayoutInformationList.vue
@@ -1,6 +1,7 @@
 <template>
   <div
     class="mb-10 w-11/12 md:w-8/12 mx-auto transition-10"
+    :id="id"
   >
     <div class="border-l-8 border-blue-400 pl-4 -ml-4 py-1 mb-4">
       <time class="text-sm md:text-base block md:w-1/6">
