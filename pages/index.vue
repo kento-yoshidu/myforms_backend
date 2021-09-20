9ebc660f80a887baed83fcfcfdce100da9497768
kento <kento@toriwatari.bird>
2021-09-20 18:36:58 +0900

    🎨 #87 スタイル変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <Tutorial/>
+  <header class="bg-yellow-200 h-96 md:h-48">
+    <h2 class="text-3xl">歯医者です。</h2>
+  </header>
 </template>
 
 <script lang="ts">
@@ -7,3 +9,23 @@ import Vue from 'vue'
 
 export default Vue.extend({})
 </script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple- system, BlinkMaxSystemFont;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
