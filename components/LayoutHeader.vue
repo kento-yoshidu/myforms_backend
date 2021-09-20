a1a2fb3562643d7fea95384a4cb8f4cd38f0b42f
kento <kento@toriwatari.bird>
2021-09-20 18:24:52 +0900

    🏃 #87 FixHeaderコンポーネントの作成

---
+++ components/LayoutHeader.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<div class="text-white md:hidden">
+			hoge
+		</div>
+		<p>hogehgoe</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'LayoutHeader'
+}
+</script>
