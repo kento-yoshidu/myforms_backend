17372131c52890ebdd90c7e24d3dbea6937120ce
kento <kento@toriwatari.bird>
2021-09-23 20:22:31 +0900

    🏃 #90 Titleコンポーネントの作成

---
+++ components/Title.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="text-gray-600 font-black">
+		<h2 class="text-4xl mb-4 md:text-7xl">{{title}}</h2>	
+		<p class="text-lg">{{message}}</p>
+	</div>
+</template>
+
+<script lang="ts">
+
+export default {
+	name: 'Title',
+	props: {
+		title: String,
+		message: String
+	}
+}
+</script>
+
+<style>
+	h2 {
+		font-weight: 900;
+	}
+</style>
