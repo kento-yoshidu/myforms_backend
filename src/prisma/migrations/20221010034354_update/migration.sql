23ff918a6cf37e55208bf9e15b03be422a6d8234
kento <kento@toriwatari.bird>
2025-06-13 21:06:20 +0900

    ⚡ Initial Commit

--- src/prisma/migrations/20221010034354_update/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "books" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "isbn" TEXT NOT NULL,
-    "rating" INTEGER NOT NULL,
-
-    CONSTRAINT "books_pkey" PRIMARY KEY ("id")
-);
